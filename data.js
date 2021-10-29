6aa437f1c2c3658120e4a04cf07cfb13da0435ca
Mahed21 <85872364+Mahed21@users.noreply.github.com>
2021-10-30 00:31:30 +0600

    Add files via upload

---
+++ data.js
@@ -0,0 +1,77 @@
+const name=[
+    {
+       name:'Sylhet Tour',
+       img:'https://images.unsplash.com/photo-1596895111956-bf1cf0599ce5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=435&q=80',
+       duraion:'7 days Tour',
+       cost:'100$',
+       date:'December 1 to December 7',
+       img1:'https://images.unsplash.com/photo-1598304205498-cf0a6c6434f4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=465&q=80'
+       ,
+       img2:'https://images.unsplash.com/photo-1627491922812-30d9e485c0fc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=774&q=80'
+       ,
+       img3:'https://images.unsplash.com/photo-1600709406925-ef2049e82151?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1032&q=80'
+    }
+    
+    
+    ,
+    {
+       name: 'Coxs Bazar Tour',
+       img:'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=873&q=80',
+      
+       duraion:'5 Days',
+       cost:'120$',
+        date:'December 15 to December 20',
+       img1:'https://images.unsplash.com/photo-1595879468810-210db077599c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+       img2:'https://images.unsplash.com/photo-1623885213877-731a59a6df1a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1032&q=80',
+       img3:'https://images.unsplash.com/photo-1623885213877-731a59a6df1a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1032&q=80',
+    }
+    ,
+    {
+       name:'Sajek Tour',
+       img:'https://images.unsplash.com/photo-1578128178799-ffbb862243c0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+       description:'',
+       duraion:'5 days',
+       cost:'80$',
+       date:'November 5 to November 10',
+       img1:'https://images.unsplash.com/photo-1594019704900-5a777996fcc4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=774&q=80',
+       img2:'https://images.unsplash.com/photo-1589307357824-541ba8e533ad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=848&q=80',
+       img3:'https://images.unsplash.com/photo-1602528495711-f52bf3988a00?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=903&q=80',
+    }
+    ,
+    {
+       name:'Sundorban Tour',
+       img:'https://images.unsplash.com/photo-1607441078533-77ba814b1e5d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80',
+       description:'',
+       duraion:'3 days',
+       cost:'70$',
+       date:'November 15 to November 20',
+       img1:'https://images.unsplash.com/photo-1612971432130-04d239cb6706?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+       img2:'https://images.unsplash.com/photo-1589556183130-530470785fab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+       img3:'https://images.unsplash.com/photo-1612103183244-7598b792bb05?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=773&q=80',
+    }
+    ,
+    {
+       name:'Saint martin',
+       img:'https://images.unsplash.com/photo-1516091877740-fde016699f2c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+       description:'',
+       duraion:'4 days',
+       cost:'100$',
+       date:'January 1 to January 5',
+        img1:'https://images.unsplash.com/photo-1535507568891-fd46651fe8c8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+       img2:'https://images.unsplash.com/photo-1515862313161-889d84b4b1fc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+       img3:'https://images.unsplash.com/photo-1622779536320-bb5f5b501a06?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=869&q=80',
+    }
+    ,
+    {
+       name:'kishurgonj tour',
+       img:'https://1.bp.blogspot.com/-agL7VOc9o9A/X7TRyvNbQ-I/AAAAAAAAGRo/qVtLpJPrxnMUpT2vY27Dr2alANtgn-MDgCLcBGAsYHQ/w680/IMG20200825130507.jpg',
+       description:'2 days',
+       duraion:'',
+       cost:'$40',
+       date:'March 1 to March 7',
+       img1:'https://lh3.googleusercontent.com/proxy/ascyM4chO5nJnBbSSvsP3jBtrgOpjDKO5DB5fOpaH6T7mZ7UqjkiIqyETWldLgrKYMEjGj1BO1UT5iTr3djUcJRCAOpWqYqK2O8mh_RVk2RHOwpGVTV9A_cGq57bLB8gB2J5qM-yvoQBC-7fhPcEgQdzsNVw_Bsw',
+       img2:'https://i.ytimg.com/vi/yChQYbxBvfI/maxresdefault.jpg',
+       img3:'https://1.bp.blogspot.com/-1oC6XkVFMPs/YQu9wpdGKLI/AAAAAAAAATI/8Umc2-CnULcnxIpTciaCvGLOuixF5eetQCLcBGAsYHQ/s720/Blog14.jpg',
+    }
+    
+    ]
